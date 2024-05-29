7fa060ba73b573c4db2882260edb97f1fa041bf9
Milan Jiříček <milan@mjiricek.eu>
2024-05-29 22:51:04 +0200

    fix budget index

--- src/pages/offline/index.tsx
+++ src/pages/offline/index.tsx
@@ -2,7 +2,6 @@ import {Page} from "@/components/ui/MainPage/Page";
 import {getBudgets} from "@/components/localStorage/budget";
 
 const Index = () => {
-    const budgets = getBudgets();
     return (
         <Page title="Offline">
             <div className="lg:w-full w-screen mx-auto lg:px-10 justify-center">
