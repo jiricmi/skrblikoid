197e3a4552a3bee1a426dc4985d2f8908e55d242
Milan Jiříček <milan@mjiricek.eu>
2024-05-30 11:27:47 +0200

    manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
+      <Head>
+          <link rel="manifest" href="/manifest.json"/>
+      </Head>
+        <body>
+        <Main/>
         <NextScript />
       </body>
     </Html>
